76d6a3c174a706811055caeb1034ce96c3b679f2
ohrydko <s1a2s3h4a656@gmail.com>
2020-04-20 21:49:49 +0300

    add script

---
+++ script.sql
@@ -0,0 +1,509 @@
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (100, 'Куркуміни', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (101, 'Рибофлавіни', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (102, 'Тартразін', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (104, 'Хіноліновий жовтий', 'Барвники', 'Середня');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (110, 'Жовтий "сонячний захід" спеціальний жовтий FCF', 'Барвники', 'Висока');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (120, 'Карміни', 'Барвники', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (122, 'Азорубін', 'Барвники', 'Висока');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (124, 'Понсо 4R', 'Барвники', 'Висока');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (129, 'Спеціальний червоний', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (131, 'Синій патентований V', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (132, 'Індигокармін', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (133, 'Діамантовий синій FCF', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (140, 'Хлорофіли', 'Барвники', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (141, 'Мідні комплекси хлорофілів', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (142, 'Зелений S', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES ('150a', 'Цукровий колер I простий', 'Барвники', 'Низька');
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (152, 'Вугілля', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (153, 'Вугілля рослинне', 'Барвники', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES ('160a', 'Каротин', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES ('160b', 'аннато', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES ('160c', 'Масло смоли паприки', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES ('160e', 'Бета-апо-8-каротинол', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (162, 'Червоний буряковий бетанін', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (163, 'Антоціани', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (164, 'Шафран', 'Барвники', 'Дуже низька');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (170, 'карбонат кальцію', 'Барвники', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (171, 'Титану двоокис', 'Барвники', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (172, 'Оксиди та гідрооксиди заліза', 'Барвники', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (175, 'золото', 'Барвники', 'Низька');
+	
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (181, 'Таніни харчові', 'Барвники', 'Дуже низька');
+	
+
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (200, 'Сорбінова кислота', 'Консерванти', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (201, 'Сорбат натрію', 'Консерванти', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (202, 'Сорбат калію', 'Консерванти', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (210, 'Бензойна кислота', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (211, 'Бензоат натрію', 'Консерванти', 'Висока');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (220, 'Сірки двоокис', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (222, 'Натрію гідросульфіт', 'Консерванти', 'Висока');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (223, 'Натрію метабісульфіт', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (224, 'Калію метабісульфіт', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (234, 'Нізін', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (235, 'Пімаріцин', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (239, 'Уротропін', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (250, 'Натрію нітрит', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (251, 'Натрію нітрат', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (252, 'Калію нітрат', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (260, 'Оцтова кислота', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (262, 'Ацетат натрію', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (263, 'Кальцію ацетат', 'Консерванти', 'Низька');
+	
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (270, 'Молочна кислота', 'Консерванти', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (285, 'Натрію тетраборат', 'Консерванти', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (290, 'Вуглецю двоокис', 'Консерванти', 'Дуже низька');
+	
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (296, 'Яблучна кислота', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (300, 'Аскорбінова кислота', 'Антиоксиданти', 'Дуже низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (301, 'Натрію аскорбат', 'Антиоксиданти', 'Немає');
+	
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (304, 'Аскорбіл пальмітат', 'Антиоксиданти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (305, 'Аскорбіл стеарат', 'Антиоксиданти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (306, 'Концентрат суміші токоферолів', 'Антиоксиданти', 'Низька');
+	
+
+
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (307, 'Альфа-токоферол', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (308, 'Гамма-токоферол ', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (309, 'Дельта-токоферол', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (316, 'Ізо-аскорбінат натрію', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (320, 'Бутил гідрооксианізол', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (321, 'Бутил гідрокситолуол', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (322, 'Лецитини', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (325, 'Лактат натрію', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (326, 'Лактат калію', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (327, 'Лактат кальцію', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (330, 'Лимонна кислота', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (331, 'Цитрати натрію', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (332, 'Цитрати калію ', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (334, 'Винна кислота', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (335, 'Тартрати натрію', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (336, 'Тартрати калію', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (338, 'Ортофосфорна кислота', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (339, 'фосфат натрію', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (340, 'Фосфати калію', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (341, 'Фосфати кальцію', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (342, 'Фосфати амонію', 'Консерванти', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (351, 'Малат калію', 'Консерванти', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (363, 'Янтарна кислота', 'Консерванти', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (386, 'Уротропін', 'Консерванти', 'Середня');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (400, 'Альгінова кислота ', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (401, 'Альгінат натрію', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (402, 'Альгінат калію', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (403, 'Альгінат амонію', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (404, 'Альгінат кальцію', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (405, 'Пропіленглікольальгінат', 'Стабілізатори', 'Низька');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (406, 'Агар', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (407, 'Карагенан', 'Стабілізатори', 'Висока');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (410, 'Камедь рожкового дерева', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (411, 'Вівсяна камедь', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (412, 'Гуарова камедь', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (413, 'Трагакант', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (414, 'Гуміарабік', 'Стабілізатори', 'Висока');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (415, 'Ксантанова камедь', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (416, 'Карайї камедь', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (417, 'Тари камедь', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (418, 'Геланова камедь', 'Стабілізатори', 'Середня');
+	
+	
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (420, 'Сорбіт та сорбітовий сироп ', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (421, 'Маніт', 'Стабілізатори', 'Висока');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (422, 'Гліцерин', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (431, 'Поліоксиетилен (40) стеарат', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (432, 'Поліоксиетиленсорбітан монолаурат Твін-20', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (433, 'Поліоксиетиленсорбітанмоноолеат, Твін-80', 'Стабілізатори', 'Немає');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (434, 'Поліоксиетиленсорбітан монопальмітат, Твін-40', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (435, 'Поліоксиетиленсорбітан моностеарат, Твін-60', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (436, 'Поліоксиетиленсорбітан тристеарат', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (440, 'Пектини', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (444, 'Ізобутират ацетат сахарози', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (445, 'Гліцеринові ефіри деревної смоли', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (450, 'Пірофосфати', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (451, 'Трифосфати', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (452, 'Поліфосфати', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (460, 'Целюлоза', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (461, 'Уротропін', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (466, 'Метилцелюлоза', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (470, 'Жирних кислот солі амонію, кальцію, натрію, магнію, калію, алюмінію', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (471, 'Моно- та дигліцериди жирних кислот', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (472, 'Ефіри гліцерину', 'Стабілізатори', 'Немає');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (473, 'Ефіри сахарози та жирних кислот', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (475, 'Ефіри полігліцеридів та жирних кислот', 'Стабілізатори', 'Низька');
+
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (476, 'Ефіри полігліцерину взаємоетерифікованих рацинолових кислот ', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (477, 'Ефіри пропіленгліколю та жирних кислот', 'Стабілізатори', 'Середня');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (481, 'Лактилати натрію', 'Стабілізатори', 'Низька');
+	
+INSERT INTO public.supplement(
+	number_supplement, name, categories, danger)
+	VALUES (492, 'Сорбітан тристеарат', 'Стабілізатори', 'Низька');
